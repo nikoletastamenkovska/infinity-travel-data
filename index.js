29e1f0f750000542ec31243e1796f4d638e2fd4f
Nikoleta Stamenkovska-Stojmirovska <nikoleta.stamenkovska@yahoo.com>
2023-09-26 19:46:15 +0200

    changes in configuration

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+// const jsonServer = require('json-server')
+// const cors = require('cors')
+// const path = require('path')
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join('db.json'))
-const middlewares = jsonServer.defaults()
+// const server = jsonServer.create()
+// const router = jsonServer.router(path.join('db.json'))
+// const middlewares = jsonServer.defaults()
 
-server.use(cors({
-    origin: 'http://localhost:3000',
-}));
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+// server.use(cors({
+//     origin: 'http://localhost:3000',
+// }));
+// server.use(jsonServer.bodyParser)
+// server.use(middlewares)
+// server.use(router)
 
-const PORT = 8000
+// const PORT = 8000
+
+// server.listen(PORT, () => {
+//     console.log(`JSON Server is running on http://localhost:${PORT}`)
+// })
+
+const jsonServer = require('json-server');
+const cors = require('cors');
+const path = require('path');
+
+const server = jsonServer.create();
+const router = jsonServer.router(path.join('db.json'));
+const middlewares = jsonServer.defaults();
+
+const allowedOrigins = ['http://localhost:3000'];
+
+server.use(
+    cors({
+        origin: function (origin, callback) {
+            if (process.env.NODE_ENV !== 'production') {
+                callback(null, true);
+            } else if (allowedOrigins.indexOf(origin) !== -1) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+    })
+);
+
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
+
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => {
-    console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+    console.log(`JSON Server is running on port ${PORT}`);
+});
